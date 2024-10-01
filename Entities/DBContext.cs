a54a0e067f11347a965d9280fc169aff64490406
Maison <maisonflynn@mail.com>
2024-10-01 17:29:51 -0400

    Colour

--- Entities/DBContext.cs
+++ Entities/DBContext.cs
@@ -18,7 +18,7 @@ namespace Ass1gnment.Entities
                 new Measurement() { MeasurementID = 2, Systolic = 142, Diastolic = 91, Date = new DateTime(2025, 12, 29) },
                 new Measurement() { MeasurementID = 3, Systolic = 131, Diastolic = 85, Date = new DateTime(2002, 11, 24) },
                 new Measurement() { MeasurementID = 4, Systolic = 122, Diastolic = 79, Date = new DateTime(1998, 05, 08) },
-                new Measurement() { MeasurementID = 5, Systolic = 188, Diastolic = 78, Date = new DateTime(1996, 02, 09) }
+                new Measurement() { MeasurementID = 5, Systolic = 118, Diastolic = 78, Date = new DateTime(1996, 02, 09) }
                 );
         }
     }
--- Entities/Measurement.cs
+++ Entities/Measurement.cs
@@ -22,25 +22,25 @@ namespace Ass1gnment.Entities
         {
             get
             {
-                if (Systolic < 120 && Diastolic < 80)
+                if (Systolic > 180 || Diastolic > 120)
                 {
-                    return "Normal";
-                }
-                else if (Systolic >= 120 && Systolic <= 129 && Diastolic < 80)
-                {
-                    return "Elevated";
-                }
-                else if ((Systolic >= 130 && Systolic <= 139) || (Diastolic >= 80 && Diastolic <= 89))
-                {
-                    return "Hypertension Stage Ⅰ";
+                    return "Hypertensive Crisis";
                 }
                 else if (Systolic >= 140 || Diastolic >= 90)
                 {
                     return "Hypertension Stage Ⅱ";
                 }
-                else if (Systolic > 180 || Diastolic > 120)
+                else if ((Systolic >= 130 && Systolic <= 139) || (Diastolic >= 80 && Diastolic <= 89))
                 {
-                    return "Hypertensive Crisis";
+                    return "Hypertension Stage Ⅰ";
+                }
+                else if (Systolic >= 120 && Systolic <= 129 && Diastolic < 80)
+                {
+                    return "Elevated";
+                }
+                else if (Systolic < 120 && Diastolic < 80)
+                {
+                    return "Normal";
                 }
                 else
                 {
